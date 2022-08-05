a7b6d7f9583b92c0711acda3cfbe62bf8abd95fe
2h311 <kcodepal@outlook.com>
2022-08-05 18:03:58 +0100

    create a test file and a .js file for our nodejs version of the script

---
+++ apibarcode.js
@@ -0,0 +1,6 @@
+/** 
+	let's write a nodejs script that'll make a request to an API to generate a  
+	GS1_128 barcode
+**/
+
+console.log("hello world")
---
+++ test.py
@@ -0,0 +1,5 @@
+# TODO: write a test for our GenerateBarcode class
+
+import pytest
+
+
